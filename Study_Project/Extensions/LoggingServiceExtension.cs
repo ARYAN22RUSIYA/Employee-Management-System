17a4c98f261ca9f49452ad13ae2a7838a4c8f607
ARYAN22RUSIYA <74164124+ARYAN22RUSIYA@users.noreply.github.com>
2025-03-16 15:30:44 +0530

    Adding Extensions to simplify Program.cs

---
+++ Study_Project/Extensions/LoggingServiceExtension.cs
@@ -0,0 +1,16 @@
+﻿using Serilog;
+
+namespace Study_Project.Extensions
+{
+    public static class LoggingServiceExtension
+    {
+        public static void AddSerilogLogging(this WebApplicationBuilder builder)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration) // ✅ Reads log settings from appsettings.json
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+        }
+    }
+}
---
+++ Study_Project/Extensions/SwaggerServiceExtension.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Study_Project.Extensions
+{
+    public static class SwaggerServiceExtension
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "Employee Management System", Version = "v1" });
+
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            return app;
+        }
+    }
+}
