61f61099909fc0c929aad92889d63a71f085ee50
ARYAN22RUSIYA <74164124+ARYAN22RUSIYA@users.noreply.github.com>
2025-04-16 00:06:06 +0530

    Created EmployeeDTO.

---
+++ Application/DTOs/EmployeeDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Study_Project.Application.DTOs
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Dob { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public int Age { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public string UpdatedBy { get; set; } = string.Empty;
+    }
+}
